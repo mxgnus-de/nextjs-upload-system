a072cc91a398d54761d5b6a7b2d72785904b9b3b
Mags007 <magnus.bache@outlook.de>
2021-12-26 01:39:51 +0100

    Update shorter.tsx
    added:
    success and error widgit;
    if success, url will be copie to clipboard

--- pages/shorter.tsx
+++ pages/shorter.tsx
@@ -1,6 +1,8 @@
 import axiosClient from 'api/axiosClient';
 import { AxiosError, AxiosResponse } from 'axios';
 import Container from 'components/Container/Container';
+import { useErrorWidgitUpdate } from 'components/Context/ErrorWidgitContext';
+import { useSuccessWidgitUpdate } from 'components/Context/SuccessWidgitContext';
 import Form from 'components/Form/Form';
 import Hyphen from 'components/Hyphen/Hyphen';
 import Input from 'components/Input/Input';
@@ -14,9 +16,11 @@ import { useRef, useState } from 'react';
 
 const Shorter: NextPage = () => {
    const [rawURL, setRawURL] = useState('');
-   const [shortURL, setshortURL] = useState('');
+   const [shortURL, setShortURL] = useState('');
    const urlInputRef = useRef<HTMLInputElement>(null);
    const shortURLRef = useRef<HTMLInputElement>(null);
+   const updateSuccessWidgit = useSuccessWidgitUpdate();
+   const updateErrorWidgit = useErrorWidgitUpdate();
 
    async function shortUrl() {
       if (!rawURL) return urlInputRef.current?.focus();
@@ -31,10 +35,19 @@ const Shorter: NextPage = () => {
       await axiosClient
          .post('/api/shorter', data, config)
          .then((res: AxiosResponse) => {
-            setshortURL(res.data);
+            const clipboard = navigator?.clipboard;
+            if (clipboard) {
+               clipboard.writeText(res.data);
+            }
+            updateSuccessWidgit?.showSuccessWidgit(
+               'URL shorted!' + (clipboard ? ' Copied to clipboard' : ''),
+            );
+            setShortURL(res.data);
          })
          .catch((err: AxiosError) => {
-            alert('Short failed\n' + err.response?.statusText);
+            updateErrorWidgit?.showErrorWidgit(
+               'Short failed\n' + err.response?.statusText,
+            );
          });
    }
 
