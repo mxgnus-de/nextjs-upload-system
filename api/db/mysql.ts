22331ac4d5d7a37e67d79094eb46886a138a04ec
Mags007 <magnus.bache@outlook.de>
2021-12-27 15:12:46 +0100

    Update mysql.ts

--- api/db/mysql.ts
+++ api/db/mysql.ts
@@ -1,5 +1,5 @@
 import Connection from './connection';
-import mysqlConfig from '../../config/mysql';
+import mysqlConfig from 'config/mysql';
 
 const connection = new Connection(
    mysqlConfig.username,
@@ -11,6 +11,8 @@ connection.connect();
 
 import FileSQL from './fileSQL';
 import ShortSQL from './shortSQL';
+import UserSQL from './userSQL';
 
 export const fileSQL = new FileSQL(connection.getConnection());
 export const shortSQL = new ShortSQL(connection.getConnection());
+export const userSQL = new UserSQL(connection.getConnection());
