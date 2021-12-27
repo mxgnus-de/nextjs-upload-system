29e6a007bb10750d1b658c316a8c1aeba50c7932
Mags007 <magnus.bache@outlook.de>
2021-12-27 15:13:07 +0100

    upload key validation added

---
+++ api/uploadKey.ts
@@ -0,0 +1,7 @@
+import { userSQL } from './db/mysql';
+
+export async function validateUploadKey(key: string): Promise<boolean> {
+   const user = await userSQL.getUser(key);
+
+   return user[0] !== undefined && user[0]?.key === key;
+}
