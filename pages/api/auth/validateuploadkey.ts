d00140113f532a77709d4951c10fe769fe250c5a
Mags007 <magnus.bache@outlook.de>
2021-12-27 15:14:43 +0100

    Create validateuploadkey.ts

---
+++ pages/api/auth/validateuploadkey.ts
@@ -0,0 +1,28 @@
+import { validateUploadKey } from 'api/uploadKey';
+import invaliduploadkey from 'api/utils/invaliduploadkey';
+import methodnotallowed from 'api/utils/methodnotallowed';
+import Cookies from 'cookies';
+import { NextApiRequest, NextApiResponse } from 'next';
+export default async function valideuploadkey(
+   req: NextApiRequest,
+   res: NextApiResponse,
+) {
+   if (req.method === 'GET') {
+      const cookies = new Cookies(req, res);
+      const uploadKey: any =
+         cookies.get('upload_key') ||
+         req.headers['authorization'] ||
+         req.body.upload_key;
+
+      const valide = await validateUploadKey(uploadKey);
+      res.statusCode === 200;
+      res.statusMessage = 'OK';
+      return res.send({
+         success: true,
+         upload_key: uploadKey,
+         valide,
+      });
+   } else {
+      return methodnotallowed(res);
+   }
+}
