3263b420c5154a6cf92504aaf65869ec18b76fc2
Mags007 <magnus.bache@outlook.de>
2021-12-27 15:13:51 +0100

    Create DashboardWrapper.tsx

---
+++ components/Dashboard/DashboardWrapper.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const DashboardWrapper = styled.div`
+   position: relative;
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   min-width: 50%;
+   margin: 0.4rem 0;
+   background-color: #26282b;
+   border-radius: 0.5rem;
+   padding: 1rem;
+`;
+
+export default DashboardWrapper;
