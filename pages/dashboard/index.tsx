fc68c2813f5fd4fdf915093b4f348233b8b4323c
Mags007 <magnus.bache@outlook.de>
2021-12-27 15:14:54 +0100

    Update index.tsx

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -14,27 +14,48 @@ import Router from 'next/router';
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import CloseIcon from '@mui/icons-material/Close';
+import DashboardWrapper from 'components/Dashboard/DashboardWrapper';
+import DashboardName from 'components/Dashboard/DashboardName';
+import DashboardButtons from 'components/Dashboard/DashboardButtons';
+import FileUpload from 'components/Dashboard/FileUpload';
+import ShortURL from 'components/Dashboard/ShortedURL';
+import Users from 'components/Dashboard/Users';
 
-interface SiteProps {
-   uploads: {
-      name: string;
-      filename: string;
-      originalfilename: string;
-      path: string;
-      mimetype: string;
-   }[];
-   shortedURLs: {
-      name: string;
-      url: string;
-   }[];
+export interface Uploads {
+   name: string;
+   filename: string;
+   originalfilename: string;
+   path: string;
+   mimetype: string;
 }
 
-const Dashboard: NextPage<SiteProps> = ({ uploads, shortedURLs }) => {
-   const [uploadFiles, setUploadFiles] = useState<typeof uploads>();
-   const [shortedURL, setShortedURL] = useState<typeof shortedURLs>();
-   const [currentDashboard, setCurrentDashboard] = useState<'files' | 'links'>(
-      'files',
-   );
+export interface ShortURL {
+   name: string;
+   url: string;
+}
+
+export interface User {
+   key: string;
+   username: string;
+}
+interface SiteProps {
+   uploads: Uploads[];
+   shortedURLs: ShortURL[];
+   initalusers: User[];
+}
+
+const Dashboard: NextPage<SiteProps> = ({
+   uploads,
+   shortedURLs,
+   initalusers,
+}) => {
+   const [uploadFiles, setUploadFiles] = useState<typeof uploads>(uploads);
+   const [shortedURL, setShortedURL] =
+      useState<typeof shortedURLs>(shortedURLs);
+   const [users, setUsers] = useState<typeof initalusers>(initalusers);
+   const [currentDashboard, setCurrentDashboard] = useState<
+      'files' | 'links' | 'users'
+   >('files');
    const [search, setSearch] = useState('');
    useEffect(() => {
       setUploadFiles(uploads);
@@ -42,8 +63,11 @@ const Dashboard: NextPage<SiteProps> = ({ uploads, shortedURLs }) => {
       const id = Router.query.id as string | undefined;
       if (id) searchChange(id);
       const site = Router.query.site as string | undefined;
+
       if (site === 'files') setCurrentDashboard('files');
       else if (site === 'links') setCurrentDashboard('links');
+      else if (site === 'users') setCurrentDashboard('users');
+
       Router.replace('/dashboard', undefined, { shallow: true });
       // eslint-disable-next-line react-hooks/exhaustive-deps
    }, [shortedURLs, uploads]);
@@ -81,6 +105,8 @@ const Dashboard: NextPage<SiteProps> = ({ uploads, shortedURLs }) => {
                onClick={() => {
                   currentDashboard === 'files'
                      ? setCurrentDashboard('links')
+                     : currentDashboard === 'links'
+                     ? setCurrentDashboard('users')
                      : setCurrentDashboard('files');
                }}
             >
@@ -105,208 +131,25 @@ const Dashboard: NextPage<SiteProps> = ({ uploads, shortedURLs }) => {
             </SearchWrapper>
             <Wrapper>
                {currentDashboard === 'files' ? (
-                  uploadFiles?.length ? (
-                     uploadFiles.map((upload, index) => {
-                        return (
-                           <Upload
-                              key={index}
-                              name={upload.name}
-                              filename={upload.filename}
-                              originalFilename={upload.originalfilename}
-                              path={upload.path}
-                              mimetype={upload.mimetype}
-                              setUploadFiles={setUploadFiles}
-                           />
-                        );
-                     })
-                  ) : (
-                     <>
-                        <h4>No uploads yet</h4>
-                        <Link href='/' passHref>
-                           <p
-                              className='button button-blue'
-                              style={{ marginTop: '5px' }}
-                           >
-                              Upload something
-                           </p>
-                        </Link>
-                     </>
-                  )
-               ) : shortedURL?.length ? (
-                  shortedURL.map((upload, index) => {
-                     return (
-                        <ShortedURL
-                           key={index}
-                           name={upload.name}
-                           url={upload.url}
-                           setShortedURL={setShortedURL}
-                        />
-                     );
-                  })
+                  <FileUpload
+                     setUploadFiles={setUploadFiles}
+                     uploadFiles={uploadFiles}
+                  />
+               ) : currentDashboard === 'links' ? (
+                  <ShortURL
+                     setShortedURL={setShortedURL}
+                     shortedURL={shortedURL}
+                  />
+               ) : currentDashboard === 'users' ? (
+                  <Users setUsers={setUsers} users={users} />
                ) : (
-                  <>
-                     <h4>No short links yet</h4>
-                     <Link href='/shorter' passHref>
-                        <p
-                           className='button button-blue'
-                           style={{ marginTop: '5px' }}
-                        >
-                           Short a url
-                        </p>
-                     </Link>
-                  </>
+                  <h1>No dashboard</h1>
                )}
             </Wrapper>
          </Container>
       </>
    );
 };
-
-const Upload = (upload: {
-   name: string;
-   filename: string;
-   originalFilename: string;
-   path: string;
-   mimetype: string;
-   setUploadFiles: any;
-}) => {
-   const updateSuccessWidgit = useSuccessWidgitUpdate();
-   const updateErrorWidgit = useErrorWidgitUpdate();
-   function deleteUpload(name: string) {
-      const confirm = window.confirm(
-         `Are you sure you want to delete ${name}?`,
-      );
-
-      if (confirm) {
-         axiosClient
-            .delete(`/api/dashboard/uploads?filename=${name}`)
-            .then(async () => {
-               updateSuccessWidgit?.showSuccessWidgit(`${name} deleted`);
-               const uploads = await axiosClient.get(
-                  server + '/api/dashboard/uploads',
-                  {
-                     withCredentials: true,
-                  },
-               );
-               if (uploads.data) {
-                  upload.setUploadFiles(uploads.data);
-               }
-            })
-            .catch((error: AxiosError) => {
-               updateErrorWidgit?.showErrorWidgit(error.message);
-            });
-      } else {
-         return updateSuccessWidgit?.showSuccessWidgit('Cancelled');
-      }
-   }
-
-   return (
-      <DashboardWrapper>
-         <DashboardName>{upload.name}</DashboardName>
-         <DashboardOriginalName>
-            {upload.originalFilename}
-         </DashboardOriginalName>
-         <DashboardButtons>
-            <Link href={'/' + upload.name} passHref>
-               <p className='button button-green'>View</p>
-            </Link>
-            <Link href={'/api/upload/' + upload.name} passHref>
-               <p className='button button-blue'>Download</p>
-            </Link>
-            <button
-               className='button button-red'
-               onClick={(e) => {
-                  deleteUpload(upload.name);
-               }}
-            >
-               Delete
-            </button>
-         </DashboardButtons>
-      </DashboardWrapper>
-   );
-};
-
-function ShortedURL(shortURL: {
-   name: string;
-   url: string;
-   setShortedURL: any;
-}) {
-   const updateSuccessWidgit = useSuccessWidgitUpdate();
-   const updateErrorWidgit = useErrorWidgitUpdate();
-   function deleteShortURL(name: string) {
-      const confirm = window.confirm(
-         `Are you sure you want to delete ${name}?`,
-      );
-
-      if (confirm) {
-         axiosClient
-            .delete(`/api/dashboard/shorts?shorturl=${name}`)
-            .then(async () => {
-               updateSuccessWidgit?.showSuccessWidgit(`${name} deleted`);
-               const shortURLs = await axiosClient.get(
-                  server + '/api/dashboard/shorts',
-                  {
-                     withCredentials: true,
-                  },
-               );
-               if (shortURLs.data) {
-                  shortURL.setShortedURL(shortURLs.data);
-               }
-            })
-            .catch((error: AxiosError) => {
-               updateErrorWidgit?.showErrorWidgit(error.message);
-            });
-      } else {
-         return updateSuccessWidgit?.showSuccessWidgit('Cancelled');
-      }
-   }
-
-   return (
-      <DashboardWrapper>
-         <DashboardName>{shortURL.name}</DashboardName>
-         <DashboardOriginalName>{shortURL.url}</DashboardOriginalName>
-         <DashboardButtons>
-            <Link href={'/links/' + shortURL.name} passHref>
-               <p className='button button-green'>View</p>
-            </Link>
-            <button
-               className='button button-red'
-               onClick={(e) => {
-                  deleteShortURL(shortURL.name);
-               }}
-            >
-               Delete
-            </button>
-         </DashboardButtons>
-      </DashboardWrapper>
-   );
-}
-
-const DashboardWrapper = styled.div`
-   position: relative;
-   display: flex;
-   justify-content: space-between;
-   align-items: center;
-   min-width: 50%;
-   margin: 0.4rem 0;
-   background-color: #26282b;
-   border-radius: 0.5rem;
-   padding: 1rem;
-`;
-
-const DashboardName = styled.div`
-   display: flex;
-`;
-
-const DashboardButtons = styled.div`
-   display: flex;
-
-   * {
-      margin: 0 0.2rem;
-   }
-`;
-
-const DashboardOriginalName = styled.div``;
 
 const SearchWrapper = styled.div`
    display: flex;
@@ -362,10 +205,20 @@ export const getServerSideProps: GetServerSideProps<SiteProps> = async (
       .catch();
    const shortedURLsData = shortedURLs.data;
 
+   const users = await axiosClient
+      .get(server + '/api/dashboard/users', {
+         headers: {
+            authorization: context.req.cookies['upload_key'] || '',
+         },
+      })
+      .catch();
+   const usersData = users.data;
+
    return {
       props: {
          uploads: uploadData,
          shortedURLs: shortedURLsData,
+         initalusers: usersData,
       },
    };
 };
