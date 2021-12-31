3587c848cc4b7534719d4ff22f2f9aa6261c8bf3
Mags007 <magnus.bache@outlook.de>
2021-12-31 18:54:31 +0100

    Create Meta.ts

---
+++ types/Meta.ts
@@ -0,0 +1,13 @@
+export interface Meta {
+   title: string;
+   description?: string;
+   keywords?: string;
+   image?: string;
+   url?: string;
+   themecolor?: string;
+   uploadmeta?: {
+      imageRawPath?: string;
+      videoRawPath?: string;
+      videomimetype?: string;
+   };
+}
