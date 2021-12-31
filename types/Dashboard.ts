b2bf54dd5a360668fe64b55c02b7258eaac68ea1
Mags007 <magnus.bache@outlook.de>
2021-12-31 18:54:22 +0100

    Create Dashboard.ts

---
+++ types/Dashboard.ts
@@ -0,0 +1,22 @@
+export interface Uploads {
+   name: string;
+   filename: string;
+   originalfilename: string;
+   path: string;
+   mimetype: string;
+}
+
+export interface ShortURL {
+   name: string;
+   url: string;
+}
+
+export interface User {
+   key: string;
+   username: string;
+}
+
+export interface Settings {
+   name: string;
+   value: 'true' | 'false';
+}
