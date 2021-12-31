955d6cda5bfcb38af32ccfca2ad3103acb548c02
Mags007 <magnus.bache@outlook.de>
2021-12-31 18:53:43 +0100

    Create github.ts

---
+++ api/utils/github.ts
@@ -0,0 +1,15 @@
+import axiosClient from 'api/axiosClient';
+import { githubrepopackageJSON, localpackageJSON } from 'config/github';
+import { readFileSync } from 'fs';
+
+async function isVersionUpToDate(): Promise<boolean> {
+   const githubPackageJSON = await axiosClient
+      .get(githubrepopackageJSON)
+      .catch();
+   const localpackage = JSON.parse(readFileSync(localpackageJSON, 'utf8'));
+   const localversion: boolean = localpackage.version;
+   const githubversion: boolean = githubPackageJSON.data.version;
+   return localversion === githubversion;
+}
+
+export { isVersionUpToDate };
