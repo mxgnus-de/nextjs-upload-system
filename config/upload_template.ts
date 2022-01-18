825e3246470ea33fb2054666de0196fe46d05cc0
Mags007 <magnus.bache@outlook.de>
2022-01-18 18:50:17 +0100

    Update upload_template.ts

--- config/upload_template.ts
+++ config/upload_template.ts
@@ -1,11 +1,11 @@
 import path from 'path';
 
 const paths = {
-   upload: path.resolve('.', 'uploads'),
-   image: path.resolve('.', 'uploads', 'images'),
-   video: path.resolve('.', 'uploads', 'videos'),
-   audio: path.resolve('.', 'uploads', 'audios'),
-   data: path.resolve('.', 'uploads', 'data'),
+   upload: path.resolve('.', 'public', 'uploads'),
+   image: path.resolve('.', 'public', 'uploads', 'images'),
+   video: path.resolve('.', 'public', 'uploads', 'videos'),
+   audio: path.resolve('.', 'public', 'uploads', 'audio'),
+   data: path.resolve('.', 'public', 'uploads', 'data'),
 };
 
 export { paths };
