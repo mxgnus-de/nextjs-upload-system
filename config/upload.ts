47e04359108d421b1a078105f0637d94e6634163
Mags007 <magnus.bache@outlook.de>
2022-01-18 22:34:05 +0100

    Create upload.ts

---
+++ config/upload.ts
@@ -0,0 +1,14 @@
+import path from 'path';
+
+/* Do not change */
+
+const paths = {
+   files: path.resolve('.', 'files'),
+   upload: path.resolve('.', 'files', 'uploads'),
+   image: path.resolve('.', 'files', 'uploads', 'images'),
+   video: path.resolve('.', 'files', 'uploads', 'videos'),
+   audio: path.resolve('.', 'files', 'uploads', 'audios'),
+   data: path.resolve('.', 'files', 'uploads', 'data'),
+};
+
+export { paths };
