6135e4f4789f194b0445930aab39b86bd55fec83
Mags007 <magnus.bache@outlook.de>
2022-01-18 23:11:20 +0100

    Update github.ts

--- config/github.ts
+++ config/github.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 
-const githubrepourl = 'https://github.com/mxgnus-de/nextjs-upload-system';
+const githubrepourl = 'https://github.mxgnus.de/nextjs-upload-system';
 const githubapirepourl: string =
    'https://api.github.com/repos/mxgnus-de/nextjs-upload-system';
 const githubrepopackageJSON: string =
