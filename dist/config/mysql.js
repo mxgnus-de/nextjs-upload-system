1b745c2e469ff44a1c01334c22106b0c91e82063
mxgnus <84542021+mxgnus-de@users.noreply.github.com>
2022-01-21 21:00:59 +0100

    Delete dist directory

--- dist/config/mysql.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const data = {
-    username: 'sql',
-    password: 'X08GpsQmSe9ak91R!',
-    host: 'mxgnus.de',
-    database: 'uploadsystem-test',
-};
-exports.default = data;
--- dist/utils/validator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateURL = void 0;
-function validateURL(url) {
-    let urlreg = /^(http(s)?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
-    return urlreg.test(url);
-}
-exports.validateURL = validateURL;
